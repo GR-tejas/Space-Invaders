66e9bd2629d32f8829cd30eeb31f6b07fa4843a6
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2024-07-05 03:21:53 +0530

    Player Class
    Added player class

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,65 +1,88 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+class player
+{
+    int health = 3, playerScore = 0;
+
+    Vector2f position = Vector2f(400.0f, 500.0f); 
+    
+    float moveSpeed = 5;
+
+public:
+
+    Texture playerTexture;
+
+    Sprite playerSprite;
+
+    void setScore(int newScore)
+    {
+        playerScore += newScore;
+    }
+
+    int getHealth()
+    {
+        return health;
+    }
+
+    int getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    int getPlayerScore()
+    {
+        return playerScore;
+    }
+
+    Vector2f getPlayerPosition()
+    {
+        return position;
+    }
+
+    void takeDamage() 
+    {
+        cout << "player takes damage\n";
+    }
+    void move()
+    {
+        cout << "player moves\n";
+    }
+    void shootBullets()
+    {
+        cout << "player shoots bullets\n";
+    }
+};
 
 int main()
 {
-    // Define the video mode (dimensions)
-    sf::VideoMode videoMode = sf::VideoMode(800, 600);
 
-    // Create a window object with specific dimensions and a title
-    sf::RenderWindow window(videoMode, "Player 1");
+    VideoMode videoMode = sf::VideoMode(800, 600);
 
-    sf::CircleShape circle1(50);
-    circle1.setFillColor(sf::Color::Green);
-    circle1.setPosition(100, 200);
+    RenderWindow window(videoMode, "Player 1");
+            
+    player player;
 
-    sf::RectangleShape Square(sf::Vector2f(60, 60));
-    Square.setFillColor(sf::Color::Red);
-    Square.setPosition(600, 100);
+    int speed = player.getMoveSpeed();
 
-    sf::Texture texture;
-    texture.loadFromFile("assets/textures/outscal_logo.png");
+    Vector2f pos = player.getPlayerPosition();
 
-    sf::Sprite outscal_sprite;
-    outscal_sprite.setTexture(texture);
 
-    outscal_sprite.setPosition(200, 400);
-    outscal_sprite.setRotation(-45);
-    outscal_sprite.setScale(0.5, 0.5);
+    cout << speed << "\n";
 
-    sf::Font font;
-    font.loadFromFile("assets/fonts/OpenSans.ttf");
-    sf::Text text("Hello SFML!", font, 50);
-    text.setFillColor(sf::Color::Red);
-
-    sf::ConvexShape triangle;
-    triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(400, 100)); // Top point
-    triangle.setPoint(1, sf::Vector2f(500, 500)); // Bottom-right point
-    triangle.setPoint(2, sf::Vector2f(300, 500)); // Bottom-left point
-    triangle.setFillColor(sf::Color::Blue);
-
+    cout << "position:" << pos.x << "," << pos.y << "\n";
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            // Check for window closure
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
 
-        // Clear the window
-        window.clear(sf::Color::Black);
-         
-        window.draw(circle1);
-         
-        window.draw(Square);
-
-        window.draw(triangle);
-
-        window.draw(text);
-
-        window.draw(outscal_sprite);
+        window.clear(sf::Color::Blue);
 
         window.display();
     }
