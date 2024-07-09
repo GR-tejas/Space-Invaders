4ce1d67b9b1c8ef214a3cf6b3539ddc199ba911e
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2024-07-09 05:47:50 +0530

    Added service locator files

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,46 @@
+#include"../Header/ServiceLocator.h"
+
+ServiceLocator::ServiceLocator()
+{
+
+}
+
+ServiceLocator::~ServiceLocator()
+{
+
+}
+
+void ServiceLocator::createServices()
+{
+
+}
+
+void ServiceLocator::clearAllServices()
+{
+
+}
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+
+}
+
+void ServiceLocator::clearAllServices()
+{
+
+}
+
+void ServiceLocator::initialize()
+{
+
+}
+
+void ServiceLocator::update()
+{
+
+}
+
+void ServiceLocator::render()
+{
+
+}
