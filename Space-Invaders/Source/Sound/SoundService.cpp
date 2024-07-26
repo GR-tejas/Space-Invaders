229ae001de28af095b2b974ddaf467312caedd11
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2024-07-27 04:14:22 +0530

    Sound Service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,27 @@
+#include "../../Header/Sound/SoundService.h"
+#include "../../Header/Global/Config.h"
+
+namespace Sound
+{
+	using namespace Global;
+
+	void SoundService::initialize()
+	{
+	}
+
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+	}
+
+	void SoundService::playSound(SoundType soundType)
+	{
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+	}
+}
