fecfead9c1bd09fd2b56b92fc43da8a03ac0bee1
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2024-07-27 03:58:40 +0530

    Spawn Multiple Barriers

--- Space-Invaders/Header/Element/ElementService.h
+++ Space-Invaders/Header/Element/ElementService.h
@@ -1,11 +1,27 @@
 #pragma once
+#include"../../Header/Element/Bunker/BunkerController.h"
+#include"../../Header/Element/Bunker/BunkerView.h"
 namespace Element
 {
+	class BunkerController;
+
 	class ElementService
 	{
 	private:
+		
+		const std::vector<Bunker::BunkerData> bunker_data_list = { Bunker::BunkerData(sf::Vector2f(130.f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(730.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1130.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1730.0f, 800.f)) };
+		std::vector<Bunker::BunkerController*> bunker_list;
+		void destroy();
 
 	public:
+		ElementService();
+		~ElementService();
+
 		void initialize();
 		void update();
 		void render();
