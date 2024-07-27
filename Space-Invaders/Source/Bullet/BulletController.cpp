67f9fab0130201dbcdeb35f413be075fc8fcae0f
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2024-07-28 01:10:05 +0530

    Bullet MVC

---
+++ Space-Invaders/Source/Bullet/BulletController.cpp
@@ -0,0 +1,20 @@
+#include"../../Header/Bullet/BulletController.h"
+
+namespace Bullet
+{
+    void BulletController::updateProjectilePosition() {}
+    void BulletController::moveUp() {}
+    void BulletController::moveDown() {}
+    void BulletController::handleOutOfBounds() {}
+
+    BulletController::BulletController(BulletType type) {}
+    BulletController::~BulletController() {}
+
+    void BulletController::initialize(sf::Vector2f position, Bullet::MovementDirection direction) {}
+
+    void BulletController::update() {}
+    void BulletController::render() {}
+
+    sf::Vector2f BulletController::getProjectilePosition() {}
+    BulletType BulletController::getBulletType() {}
+}
